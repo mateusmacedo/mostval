e51dd244fde7a0608066fbb56bc6c7c0223b2ddf
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-12-29 16:28:04 -0300

    refactor(entity): ♻️ update Timestamper interface to a generic Timestamp type

--- packages/common/src/lib/entity.ts
+++ packages/common/src/lib/entity.ts
@@ -1,18 +1,18 @@
-export interface Timestamper {
-  createdAt:  string | Date | undefined;
-  updatedAt: Date | string | undefined;
+export interface Timestamp<TTimestamp = Date | string | number | undefined> {
+  createdAt:  TTimestamp;
+  updatedAt: TTimestamp;
 }
 
 export interface Entity<TIdentifier> {
   readonly id: TIdentifier;
-  readonly timestamper: Timestamper;
+  readonly timestamper: Timestamp;
   readonly version: number;
 }
 
 export abstract class BaseEntity<TIdentifier> implements Entity<TIdentifier> {
   constructor(
     public readonly id: TIdentifier,
-    public readonly timestamper: Timestamper,
+    public readonly timestamper: Timestamp,
     public readonly version: number
   ) {}
 }

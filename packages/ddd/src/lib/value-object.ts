b43e446d6bd5910d00ddb6da06702ba21ce06f4a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-03 20:45:32 -0300

    feat(value-object): ✨ introduce IValueObject interface and abstract ValueObject class
    - Added IValueObject interface to define the structure for value objects, including methods for equality comparison and string representation.
    - Implemented an abstract ValueObject class that provides a base for creating value objects with value encapsulation and equality logic.
    - Enhanced the equals method to handle both primitive and object comparisons, ensuring robust equality checks.
    - Included a toString method for better string representation of value objects.

---
+++ packages/ddd/src/lib/value-object.ts
@@ -0,0 +1,30 @@
+export interface IValueObject<T> {
+  value: T;
+  equals(other: IValueObject<T>): boolean;
+  toString(): string;
+}
+
+export abstract class ValueObject<T> implements IValueObject<T> {
+    public readonly value: T;
+
+    constructor(value: T) {
+      this.value = value;
+    }
+
+    public equals(other: IValueObject<T>): boolean {
+      if (other === null || other === undefined) {
+        return false;
+      }
+      if (typeof this.value === 'object' && typeof other.value === 'object') {
+        return JSON.stringify(this.value) === JSON.stringify(other.value);
+      }
+      return this.value === other.value;
+    }
+
+    public toString(): string {
+      if (typeof this.value === 'object') {
+        return JSON.stringify(this.value);
+      }
+      return String(this.value);
+    }
+  }

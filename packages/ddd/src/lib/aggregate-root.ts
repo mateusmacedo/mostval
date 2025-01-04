ccd4e3295e3cb6ec19ef2e6acd36ba7211c6dc03
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-04 14:12:26 -0300

    feat(ddd): ✨ introduce IAggregate interface and AggregateRoot class
    - Added IAggregate interface to define a structure for aggregate roots, incorporating identity, versioning, and timestamping.
    - Implemented AggregateRoot class as an abstract base for aggregate roots, providing a constructor for essential properties such as id, version, createdAt, updatedAt, and type.
    - This addition enhances the domain-driven design capabilities of the application, laying the groundwork for aggregate management.

---
+++ packages/ddd/src/lib/aggregate-root.ts
@@ -0,0 +1,9 @@
+import { IIdentity, IVersion, ITimestamper } from '@mostval/common';
+
+export interface IAggregate<TIdentifier, TVersion> extends IIdentity<TIdentifier>, IVersion<TVersion>, ITimestamper {
+    readonly type: string;
+}
+
+export abstract class AggregateRoot<TIdentifier, TVersion> implements IAggregate<TIdentifier, TVersion> {
+    constructor(readonly id: TIdentifier, readonly version: TVersion, readonly createdAt: Date, readonly updatedAt: Date, readonly type: string) {}
+}

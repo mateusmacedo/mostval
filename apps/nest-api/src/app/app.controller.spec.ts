369ec0e9a23cf3d4347230f0092ae427e5abc586
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-05 01:05:18 -0300

    test(app): 🧪 enhance AppController tests with pipeline error handling
    - Introduced a mocked pipeline in AppController tests to simulate both successful and failed executions.
    - Added a new test case to verify that the AppController correctly handles errors thrown by the pipeline.
    - These changes improve test coverage and ensure robust error handling in the application.

--- apps/nest-api/src/app/app.controller.spec.ts
+++ apps/nest-api/src/app/app.controller.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { AppController } from './app.controller'
-import { AppService, PIPELINE_SYMBOL } from './app.service'
-import { of } from 'rxjs'
+import { AppService, DataItem, PIPELINE_SYMBOL } from './app.service'
+import { of, throwError } from 'rxjs'
+import { Pipeline } from '@mostval/pipeline'
 
 describe('AppController', () => {
   let app: TestingModule
+  let pipeline: jest.Mocked<Pipeline<DataItem, DataItem>>
 
   beforeAll(async () => {
+    pipeline = {
+      execute: jest.fn().mockReturnValue(of({ message: 'Execute App Service' }))
+    } as unknown as jest.Mocked<Pipeline<DataItem, DataItem>>
+
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService, { provide: PIPELINE_SYMBOL, useValue: {
-        execute: jest.fn().mockReturnValue(of({ message: 'Execute App Service' }))
-      } }],
+      providers: [AppService, { provide: PIPELINE_SYMBOL, useValue: pipeline }],
     }).compile()
   })
 
@@ -21,6 +25,12 @@ describe('AppController', () => {
       const appController = app.get<AppController>(AppController)
       const result = await appController.executeAppService()
       expect(result).toEqual({ message: 'Execute App Service' })
+    })
+
+    it('should throw an error if the pipeline fails', async () => {
+      pipeline.execute.mockImplementationOnce(() => throwError(() => new Error('Pipeline failed')))
+      const appController = app.get<AppController>(AppController)
+      await expect(appController.executeAppService()).rejects.toThrow('Pipeline failed')
     })
   })
 })

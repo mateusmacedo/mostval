dc9f169b9c9af49a123117baaaac9d45d4a08c3f
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-05 21:36:37 -0300

    feat(common): ✨ enhance message handling with MessageStore implementation
    - Refactored the Message class to use IPayload for payload management.
    - Introduced MessageStore interface and InMemoryMessageStore class for message storage and retrieval.
    - Added methods for adding, getting, and removing messages based on criteria, improving message management capabilities.
    - Enhanced the overall structure and clarity of message handling in the application.

--- packages/common/src/lib/message.ts
+++ packages/common/src/lib/message.ts
@@ -5,19 +5,45 @@ export interface IPayload {
 export interface IMetadata {
     readonly schema: string;
     readonly type: string;
-    readonly id: string;
-    readonly correlationId: string;
     readonly timestamp: number;
-    readonly issuer?: string;
-    readonly [key: string]: unknown;
+    readonly id: string;
 }
 
-export abstract class Message<T> {
-    readonly payload: T;
+export abstract class Message {
+    readonly payload: IPayload;
     readonly metadata: IMetadata;
 
-    constructor(payload: T, metadata: IMetadata) {
+    constructor(payload: IPayload, metadata: IMetadata) {
         this.payload = payload
         this.metadata = metadata
+    }
+}
+
+export interface MessageStore {
+    add(message: Message): void;
+    get(criteria: Partial<IPayload> | Partial<IMetadata>): Message[];
+    remove(criteria: Partial<IPayload> | Partial<IMetadata>): void;
+}
+
+export class InMemoryMessageStore implements MessageStore {
+    private messages: Message[] = [];
+
+    add(message: Message): void {
+        this.messages.push(message);
+    }
+
+    get(criteria: Partial<IPayload> | Partial<IMetadata>): Message[] {
+        return this.messages.filter(message => this.matches(message, criteria));
+    }
+
+    remove(criteria: Partial<IPayload> | Partial<IMetadata>): void {
+        this.messages = this.messages.filter(message => !this.matches(message, criteria));
+    }
+
+    private matches(message: Message, criteria: Partial<IPayload> | Partial<IMetadata>): boolean {
+        return Object.keys(criteria).some(key =>
+            (typeof message.payload === 'object' && message.payload !== null && key in message.payload && (message.payload as any)[key] === (criteria as any)[key]) ||
+            (typeof message.metadata === 'object' && message.metadata !== null && key in message.metadata && (message.metadata as any)[key] === (criteria as any)[key])
+        );
     }
 }

697fec2f250bb269297cbb8cbeabec79ae78da7f
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-05 23:53:29 -0300

    refactor(users): 🔧 remove AppService and introduce UserService for enhanced user management
    - Deleted the obsolete AppService file to streamline the codebase.
    - Added UserService to encapsulate user-related operations such as creating, finding, updating, and deleting users.
    - This refactor improves the organization of user management functionalities, aligning with recent modularity enhancements in the user management system.

---
+++ apps/nest-api-users/src/app/user.service.ts
@@ -0,0 +1,36 @@
+import { User, UserProps, IUserRepository, IUserFactory, TUserCriteria, ChangeUserCredentialsCommand } from '@mostval/users'
+import { Injectable, Inject } from '@nestjs/common'
+import { USER_FACTORY, USER_REPOSITORY } from '@mostval/users'
+import { ICredentials } from '@mostval/iam'
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(USER_FACTORY) private readonly userFactory: IUserFactory,
+    @Inject(USER_REPOSITORY) private readonly userRepository: IUserRepository,
+  ) {}
+
+  async createUser(userProps: UserProps): Promise<User<UserProps>> {
+    const user = this.userFactory.create(userProps)
+    return this.userRepository.save(user)
+  }
+
+  async findUser(criteria: TUserCriteria[]): Promise<User<UserProps>[]> {
+    return this.userRepository.find(criteria)
+  }
+
+  async deleteUser(criteria: TUserCriteria[]): Promise<void> {
+    return this.userRepository.remove(criteria)
+  }
+
+  async updateUserCredentials(command: ChangeUserCredentialsCommand): Promise<User<UserProps>> {
+    const criteria: TUserCriteria[] = [['id', command.metadata['id']]]
+    const users = await this.userRepository.find(criteria)
+    if (users.length === 0) {
+      throw new Error('User not found')
+    }
+    const user = users[0]
+    user.changeCredentials(command)
+    return this.userRepository.save(user)
+  }
+}

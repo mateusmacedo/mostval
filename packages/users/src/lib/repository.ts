1edf97fbbc4f2715a2049fcebcda1d05b7e23caa
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-05 23:51:39 -0300

    refactor(users): 🔧 update InMemoryUserRepository to return Promises for asynchronous operations
    - Refactored the InMemoryUserRepository interface to return Promises for save, find, and remove methods, enhancing the handling of asynchronous operations.
    - Updated the implementation of these methods to resolve Promises, improving consistency with modern JavaScript practices.
    - This change aligns with ongoing improvements in the user management system, ensuring better integration with asynchronous workflows.

--- packages/users/src/lib/repository.ts
+++ packages/users/src/lib/repository.ts
@@ -2,24 +2,28 @@ import { User, UserProps } from './model'
 
 export type TUserCriteria = [keyof UserProps, UserProps[keyof UserProps]]
 
-export interface UserRepository {
-  save(user: User<UserProps>): void
-  find(criteria: TUserCriteria[]): User<UserProps>[]
-  remove(criteria: TUserCriteria[]): void
+export interface IUserRepository {
+  save(user: User<UserProps>): Promise<User<UserProps>>
+  find(criteria: TUserCriteria[]): Promise<User<UserProps>[]>
+  remove(criteria: TUserCriteria[]): Promise<void>
 }
 
-export class InMemoryUserRepository implements UserRepository {
+export class InMemoryUserRepository implements IUserRepository {
   private users: User<UserProps>[] = []
 
-  save(user: User<UserProps>): void {
+  save(user: User<UserProps>): Promise<User<UserProps>> {
     this.users.push(user)
+    return Promise.resolve(user)
   }
 
-  find(criteria: TUserCriteria[]): User<UserProps>[] {
-    return this.users.filter((user) => criteria.every(([key, value]) => user.props[key] === value))
+  find(criteria: TUserCriteria[]): Promise<User<UserProps>[]> {
+    return Promise.resolve(this.users.filter((user) => criteria.every(([key, value]) => user.props[key] === value)))
   }
 
-  remove(criteria: TUserCriteria[]): void {
+  remove(criteria: TUserCriteria[]): Promise<void> {
     this.users = this.users.filter((item) => !criteria.every(([key, value]) => item.props[key] === value))
+    return Promise.resolve()
   }
 }
+
+export const USER_REPOSITORY = Symbol('IUserRepository')

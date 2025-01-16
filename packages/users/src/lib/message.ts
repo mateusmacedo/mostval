aeed2d93a14d87f623a2ca681a4202f7777bd523
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 07:52:19 -0300

    refactor(message): 🛠️ enhance type safety in message classes
    - Updated the CreateUserCommand, UserCreatedEvent, ChangeUserCredentialsCommand, and UserCredentialsChangedEvent classes to utilize generics for improved type safety.
    - Changed the payload types to Partial<UserProps> and ICredentials, ensuring better alignment with TypeScript's type system.
    - These modifications enhance the maintainability and clarity of the messaging system, promoting more robust message handling.

--- packages/users/src/lib/message.ts
+++ packages/users/src/lib/message.ts
@@ -2,25 +2,25 @@ import { IMetadata, Message } from '@mostval/common'
 import { UserProps } from './model'
 import { ICredentials } from '@mostval/iam'
 
-export class CreateUserCommand extends Message {
+export class CreateUserCommand extends Message<Partial<UserProps>> {
   constructor(payload: Partial<UserProps>, metadata: IMetadata) {
     super(payload, metadata)
   }
 }
 
-export class UserCreatedEvent extends Message {
+export class UserCreatedEvent extends Message<UserProps> {
   constructor(payload: UserProps, metadata: IMetadata) {
     super(payload, metadata)
   }
 }
 
-export class ChangeUserCredentialsCommand extends Message {
+export class ChangeUserCredentialsCommand extends Message<ICredentials> {
   constructor(payload: ICredentials, metadata: IMetadata) {
     super(payload, metadata)
   }
 }
 
-export class UserCredentialsChangedEvent extends Message {
+export class UserCredentialsChangedEvent extends Message<ICredentials> {
   constructor(payload: ICredentials, metadata: IMetadata) {
     super(payload, metadata)
   }

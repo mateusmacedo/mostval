62ea1b7fc310518e9ff05963e68a19b90b590877
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 04:14:12 -0300

    feat(common): ✨ enhance error handling by standardizing error codes and refactoring error classes
    - Updated AbstractError to use ErrorCode type for the error code property, improving type safety and clarity.
    - Introduced ProcessingError and updated existing error classes (DependencyError, InternalError, NotFoundError, ValidationError) to assign appropriate error codes.
    - Removed InvalidDataError class to streamline error handling.
    - Enhanced error management by adding ProcessingError to the error exports, ensuring a consistent approach to error handling across the application.

--- packages/common/src/lib/error/not-found-error.ts
+++ packages/common/src/lib/error/not-found-error.ts
@@ -1,8 +1,9 @@
+import { ErrorCode } from '@mostval/common';
 import { AbstractError, ErrorMessage } from './abstract-error';
 
 export class NotFoundError extends AbstractError<ErrorMessage> {
   constructor(message: ErrorMessage) {
     super(message);
-    this.name = 'NotFoundError';
+    this.code = ErrorCode.NOT_FOUND;
   }
 }

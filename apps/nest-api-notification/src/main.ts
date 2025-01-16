96e51f0a735cb27fbc38b39583d0b2a8b3ea844c
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 13:42:25 -0300

    style(nest-api-notification): 🎨 standardize code formatting and improve consistency
    - Updated ESLint and Jest configuration files to ensure consistent formatting by adding missing semicolons and adjusting array formatting for tags and arguments.
    - Refactored TypeScript configuration to enhance readability by formatting the include array.
    - Modified Webpack configuration for consistent semicolon usage.
    - Improved main application file formatting for better readability and consistency in code style.
    
    These changes contribute to a cleaner and more maintainable codebase, enhancing overall code quality across the nest-api-notification application.

--- apps/nest-api-notification/src/main.ts
+++ apps/nest-api-notification/src/main.ts
@@ -3,17 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import { NotificationModule } from './app/notification.module'
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NotificationModule } from './app/notification.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule)
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  const port = process.env['PORT'] || 3000
-  await app.listen(port)
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
+  const app = await NestFactory.create(NotificationModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env['PORT'] || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
-bootstrap()
+bootstrap();

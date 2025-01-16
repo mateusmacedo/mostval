b53f2fea38101f9533639ccb8d0fde29a278134e
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 02:17:01 -0300

    feat(notification): ✨ implement NotificationBuilder class for streamlined notification creation
    - Added the NotificationBuilder class to facilitate the construction of Notification instances with customizable channels, content, and status.
    - This builder pattern enhances the usability and flexibility of the notification system, allowing for more dynamic and readable notification creation.
    - The NotificationBuilder implements the INotificationBuilder interface, ensuring type safety and consistency in building notifications.

--- packages/notification/src/lib/notification/domain/builder.ts
+++ packages/notification/src/lib/notification/domain/builder.ts
@@ -6,3 +6,32 @@ export interface INotificationBuilder<T> {
     withContent(content: T): INotificationBuilder<T>;
     withStatus(status: NotificationStatus): INotificationBuilder<T>;
 }
+
+export class NotificationBuilder<T> implements INotificationBuilder<T> {
+    private channels: INotificationChannel<unknown>[] = [];
+    private content!: T;
+    private status: NotificationStatus = NotificationStatus.CREATED;
+
+    build(): Notification<T> {
+        return new Notification<T>({
+            channels: this.channels,
+            content: this.content,
+            status: this.status
+        });
+    }
+
+    withChannels(channels: INotificationChannel<unknown>[]): INotificationBuilder<T> {
+        this.channels = channels;
+        return this;
+    }
+
+    withContent(content: T): INotificationBuilder<T> {
+        this.content = content;
+        return this;
+    }
+
+    withStatus(status: NotificationStatus): INotificationBuilder<T> {
+        this.status = status;
+        return this;
+    }
+}

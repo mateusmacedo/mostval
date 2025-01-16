5795bfb1f3fcbd887663965d40cf242b3312e6f7
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 15:28:54 -0300

    refactor(notification): 🛠️ enhance NotificationBuilder tests and type safety
    - Refactored the NotificationBuilder tests to improve clarity and maintainability by standardizing formatting and removing unnecessary generics.
    - Updated the INotificationBuilder interface to simplify the channels parameter, enhancing type safety.
    - Ensured consistent handling of channels and content in the NotificationBuilder, improving the overall structure and reliability of the notification building process.
    - These changes contribute to a more organized and robust notification module, facilitating better integration of notification functionalities.

--- packages/notification/src/lib/notification/domain/builder.ts
+++ packages/notification/src/lib/notification/domain/builder.ts
@@ -6,9 +6,7 @@ import {
 import { IBuilder } from '@mostval/common';
 
 export interface INotificationBuilder<T> extends IBuilder<Notification<T>> {
-  withChannels(
-    channels: INotificationChannel<unknown>[]
-  ): INotificationBuilder<T>;
+  withChannels(channels: INotificationChannel[]): INotificationBuilder<T>;
   withContent(content: T): INotificationBuilder<T>;
   withStatus(status: NotificationStatus): INotificationBuilder<T>;
 }
@@ -16,7 +14,7 @@ export interface INotificationBuilder<T> extends IBuilder<Notification<T>> {
 export const INotificationBuilderSymbol = Symbol('INotificationBuilder');
 
 export class NotificationBuilder<T> implements INotificationBuilder<T> {
-  private channels: INotificationChannel<unknown>[] = [];
+  private channels: INotificationChannel[] = [];
   private content!: T;
   private status: NotificationStatus = NotificationStatus.CREATED;
 
@@ -32,19 +30,17 @@ export class NotificationBuilder<T> implements INotificationBuilder<T> {
       throw new Error('Channels are required');
     }
     const instance = new Notification<T>({
-      channels: this.channels,
+      channels: this.channels as INotificationChannel[],
       content: this.content,
       status: this.status,
     });
-    this.channels = undefined as unknown as INotificationChannel<unknown>[];
+    this.channels = undefined as unknown as INotificationChannel[];
     this.content = undefined as unknown as T;
     this.status = undefined as unknown as NotificationStatus;
     return instance;
   }
 
-  withChannels(
-    channels: INotificationChannel<unknown>[]
-  ): INotificationBuilder<T> {
+  withChannels(channels: INotificationChannel[]): INotificationBuilder<T> {
     this.channels = channels;
     return this;
   }

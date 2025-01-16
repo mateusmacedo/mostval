a724238606c0b1745617f12ae1437ebc91d5267a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 14:55:10 -0300

    refactor(notification): 🛠️ add symbol for INotificationPersisterRepository interface
    - Introduced a new symbol for the INotificationPersisterRepository interface to enhance type safety and facilitate dependency injection.
    - This change improves the modularity of the notification system, making it easier to manage and integrate notification persistence functionalities.

--- packages/notification/src/lib/notification/domain/repository.ts
+++ packages/notification/src/lib/notification/domain/repository.ts
@@ -1,5 +1,9 @@
 import { Notification } from './notification';
 
 export interface INotificationPersisterRepository {
-    persist<T>(notification: Notification<T>): Promise<Notification<T>>
+  persist<T>(notification: Notification<T>): Promise<Notification<T>>;
 }
+
+export const INotificationPersisterRepositorySymbol = Symbol(
+  'INotificationPersisterRepository'
+);

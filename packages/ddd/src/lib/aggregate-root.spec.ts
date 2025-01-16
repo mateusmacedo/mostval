cded165cbec5d476b24fa3ac4968be2b15188aa5
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 01:33:34 -0300

    refactor(aggregate-root): 🛠️ remove 'type' parameter from AggregateRoot constructor
    - Simplified the AggregateRoot class by removing the 'type' parameter from its constructor, streamlining the instantiation process.
    - Updated the AggregateRootSpec test class accordingly to reflect this change, ensuring that the tests remain valid and functional.
    - This refactor enhances code clarity and reduces complexity in the aggregate root implementation.

--- packages/ddd/src/lib/aggregate-root.spec.ts
+++ packages/ddd/src/lib/aggregate-root.spec.ts
@@ -1,14 +1,14 @@
 import { AggregateRoot } from './aggregate-root';
 
 class AggregateRootSpec extends AggregateRoot<string, number> {
-    constructor(id: string, version: number, createdAt: Date, updatedAt: Date, type: string) {
-        super(id, version, createdAt, updatedAt, type);
+    constructor(id: string, version: number, createdAt: Date, updatedAt: Date) {
+        super(id, version, createdAt, updatedAt);
     }
 }
 
 describe('AggregateRoot', () => {
     it('should be able to create an aggregate root', () => {
-        const aggregateRoot = new AggregateRootSpec('123', 1, new Date(), new Date(), 'test');
+        const aggregateRoot = new AggregateRootSpec('123', 1, new Date(), new Date());
         expect(aggregateRoot).toBeDefined();
     });
 });
--- packages/ddd/src/lib/aggregate-root.ts
+++ packages/ddd/src/lib/aggregate-root.ts
@@ -1,9 +1,8 @@
 import { IIdentity, IVersion, ITimestamper } from '@mostval/common';
 
 export interface IAggregate<TIdentifier, TVersion> extends IIdentity<TIdentifier>, IVersion<TVersion>, ITimestamper {
-    readonly type: string;
 }
 
 export abstract class AggregateRoot<TIdentifier, TVersion> implements IAggregate<TIdentifier, TVersion> {
-    constructor(readonly id: TIdentifier, readonly version: TVersion, readonly createdAt: Date, readonly updatedAt: Date, readonly type: string) {}
+    constructor(readonly id: TIdentifier, readonly version: TVersion, readonly createdAt: Date, readonly updatedAt: Date) {}
 }

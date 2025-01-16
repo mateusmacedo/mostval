4e9e0365f1df5de0819fb6b74014c638970b8901
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 15:55:34 -0300

    test(notification): 🧪 enhance unit tests for NotificationService
    - Updated unit tests for NotificationService to include mocks for CreateNewNotification and PersistNotification, improving test isolation and reliability.
    - Added new test cases for createAndPersistNotification method, ensuring correct behavior for both successful execution and error handling.
    - Enhanced type safety by incorporating TCreateNewNotificationInput in the tests, contributing to better maintainability and clarity within the notification module.

--- apps/nest-api-notification/src/app/notification.service.spec.ts
+++ apps/nest-api-notification/src/app/notification.service.spec.ts
@@ -1,20 +1,102 @@
-import { Test } from '@nestjs/testing'
-import { NotificationService } from './notification.service'
+import {
+  CreateNewNotification,
+  CreateNewNotificationSymbol,
+  PersistNotification,
+  PersistNotificationSymbol,
+  TCreateNewNotificationInput,
+} from '@mostval/notification';
+import { Test } from '@nestjs/testing';
+import { NotificationService } from './notification.service';
+
+const mockPersistNotification = {
+  execute: jest.fn(),
+};
+
+const mockCreateNewNotification = {
+  execute: jest.fn(),
+};
 
 describe('NotificationService', () => {
-  let service: NotificationService
+  let createNewNotification: CreateNewNotification;
+  let persistNotification: PersistNotification;
+  let service: NotificationService;
 
   beforeAll(async () => {
     const app = await Test.createTestingModule({
-      providers: [NotificationService],
-    }).compile()
+      providers: [
+        {
+          provide: CreateNewNotificationSymbol,
+          useValue: mockCreateNewNotification,
+        },
+        {
+          provide: PersistNotificationSymbol,
+          useValue: mockPersistNotification,
+        },
+        NotificationService,
+      ],
+    }).compile();
 
-    service = app.get<NotificationService>(NotificationService)
-  })
+    createNewNotification = app.get<CreateNewNotification>(
+      CreateNewNotificationSymbol
+    );
+    persistNotification = app.get<PersistNotification>(
+      PersistNotificationSymbol
+    );
+    service = app.get<NotificationService>(NotificationService);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(createNewNotification).toBeDefined();
+    expect(persistNotification).toBeDefined();
+    expect(service).toBeDefined();
+  });
 
   describe('execute', () => {
-    it('should return "Notification API"', () => {
-      expect(service.execute()).toEqual({ message: 'Notification API' })
-    })
-  })
-})
+    it('should create and persist a notification', async () => {
+      const data: TCreateNewNotificationInput<string> = {
+        channels: [],
+        content: 'Notification API',
+      };
+      mockCreateNewNotification.execute.mockResolvedValue({
+        message: 'Notification API',
+      });
+      mockPersistNotification.execute.mockResolvedValue({
+        message: 'Notification API',
+      });
+      const result = await service.createAndPersistNotification(data);
+      expect(result).toEqual({
+        message: 'Notification API',
+      });
+    });
+
+    it('should throw an error if the builder throws an error', () => {
+      mockCreateNewNotification.execute.mockImplementation(() => {
+        throw new Error('Error');
+      });
+      const data: TCreateNewNotificationInput<string> = {
+        channels: [],
+        content: 'Notification API',
+      };
+      expect(service.createAndPersistNotification(data)).rejects.toThrow(
+        'Error'
+      );
+    });
+
+    it('should throw an error if the persist notification throws an error', () => {
+      mockPersistNotification.execute.mockImplementation(() => {
+        throw new Error('Error');
+      });
+      const data: TCreateNewNotificationInput<string> = {
+        channels: [],
+        content: 'Notification API',
+      };
+      expect(service.createAndPersistNotification(data)).rejects.toThrow(
+        'Error'
+      );
+    });
+  });
+});

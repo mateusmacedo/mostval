5a772eae69a4d4589c307c0a7c7faebab121ffaf
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 01:35:52 -0300

    feat(notification): ✨ implement Notification class and enums for notification management
    - Removed the old notification.ts file and replaced it with a new notification.ts file that defines the Notification class and related enums.
    - Introduced NotificationStatus and NotificationChannelType enums to represent various notification states and channel types.
    - Created the INotificationChannel interface and TNotificationProps type to structure notification channel data and properties.
    - Enhanced the Notification class with methods for value retrieval, equality checks, and string/JSON representation, improving the overall functionality and clarity of the notification system.

---
+++ packages/notification/src/lib/notification/domain/notification.ts
@@ -0,0 +1,70 @@
+import { ITimestamper, TIdentity } from '@mostval/common';
+import { AggregateRoot, IValueObject } from '@mostval/ddd';
+
+export enum NotificationStatus {
+    CREATED = 'CREATED',
+    SCHEDULED = 'SCHEDULED',
+    ENQUEUED = 'ENQUEUED',
+    SENT = 'SENT',
+    FAILED = 'FAILED',
+    CANCELLED = 'CANCELLED',
+    DELIVERED = 'DELIVERED'
+}
+
+export enum NotificationChannelType {
+    EMAIL = 'EMAIL',
+    SMS = 'SMS',
+    PUSH = 'PUSH'
+}
+
+export interface INotificationChannel<T> {
+    type: NotificationChannelType
+    address: IValueObject<T>
+}
+
+export type TNotificationProps = {
+    templateId: TIdentity<string, number>
+    channels: INotificationChannel<unknown>[]
+    content: string
+}
+
+export class Notification extends AggregateRoot<string, number> implements IValueObject<TNotificationProps> {
+    constructor(
+        protected props: TNotificationProps,
+        identity?: TIdentity<string, number>,
+        timestamps?: ITimestamper
+    ) {
+        super(
+            identity?.id ?? '',
+            identity?.version ?? 0,
+            timestamps?.createdAt ?? new Date(),
+            timestamps?.updatedAt ?? new Date()
+        );
+    }
+    getValue(): TNotificationProps {
+        return this.props;
+    }
+
+    equals(other: IValueObject<TNotificationProps>): boolean {
+        if (!(other instanceof Notification)) {
+            return false;
+        }
+        return (
+            this.props.templateId === other.getValue().templateId &&
+            this.props.content === other.getValue().content &&
+            this.props.channels.length === other.getValue().channels.length &&
+            this.props.channels.every((channel, index) =>
+                channel.type === other.getValue().channels[index].type &&
+                channel.address.equals(other.getValue().channels[index].address)
+            )
+        );
+    }
+
+    asString(): string {
+        return `Notification: ${this.props.templateId} ${this.props.content} ${this.props.channels.map(channel => channel.type).join(', ')}`;
+    }
+
+    asJSON(): string {
+        return JSON.stringify(this.props);
+    }
+}

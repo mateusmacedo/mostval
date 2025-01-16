306320261955514cc7d93e97a44a8de34bdbb871
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 14:55:22 -0300

    refactor(notification): 🛠️ improve NotificationBuilder structure and formatting
    - Refactored the NotificationBuilder class to enhance readability by standardizing method formatting and improving code structure.
    - Introduced a new symbol for the NotificationBuilder to facilitate better type safety and integration.
    - These changes contribute to a more maintainable and organized notification module, improving clarity in the notification building process.

--- packages/notification/src/lib/notification/domain/builder.ts
+++ packages/notification/src/lib/notification/domain/builder.ts
@@ -1,49 +1,63 @@
-import { INotificationChannel, Notification, NotificationStatus } from './notification';
+import {
+  INotificationChannel,
+  Notification,
+  NotificationStatus,
+} from './notification';
 import { IBuilder } from '@mostval/common';
 
 export interface INotificationBuilder<T> extends IBuilder<Notification<T>> {
-    withChannels(channels: INotificationChannel<unknown>[]): INotificationBuilder<T>;
-    withContent(content: T): INotificationBuilder<T>;
-    withStatus(status: NotificationStatus): INotificationBuilder<T>;
+  withChannels(
+    channels: INotificationChannel<unknown>[]
+  ): INotificationBuilder<T>;
+  withContent(content: T): INotificationBuilder<T>;
+  withStatus(status: NotificationStatus): INotificationBuilder<T>;
 }
+
+export const INotificationBuilderSymbol = Symbol('INotificationBuilder');
 
 export class NotificationBuilder<T> implements INotificationBuilder<T> {
-    private channels: INotificationChannel<unknown>[] = [];
-    private content!: T;
-    private status: NotificationStatus = NotificationStatus.CREATED;
+  private channels: INotificationChannel<unknown>[] = [];
+  private content!: T;
+  private status: NotificationStatus = NotificationStatus.CREATED;
 
-    build(): Notification<T> {
-        if (this.content === undefined || this.content === null) {
-            throw new Error('Content is required');
-        }
-        if (this.channels === undefined || this.channels === null || this.channels.length === 0) {
-            throw new Error('Channels are required');
-        }
-        const instance = new Notification<T>({
-            channels: this.channels,
-            content: this.content,
-            status: this.status
-        });
-        this.channels = undefined as unknown as INotificationChannel<unknown>[];
-        this.content = undefined as unknown as T;
-        this.status = undefined as unknown as NotificationStatus;
-        return instance;
+  build(): Notification<T> {
+    if (this.content === undefined || this.content === null) {
+      throw new Error('Content is required');
     }
-
-    withChannels(channels: INotificationChannel<unknown>[]): INotificationBuilder<T> {
-
-        this.channels = channels;
-        return this;
+    if (
+      this.channels === undefined ||
+      this.channels === null ||
+      this.channels.length === 0
+    ) {
+      throw new Error('Channels are required');
     }
+    const instance = new Notification<T>({
+      channels: this.channels,
+      content: this.content,
+      status: this.status,
+    });
+    this.channels = undefined as unknown as INotificationChannel<unknown>[];
+    this.content = undefined as unknown as T;
+    this.status = undefined as unknown as NotificationStatus;
+    return instance;
+  }
 
-    withContent(content: T): INotificationBuilder<T> {
+  withChannels(
+    channels: INotificationChannel<unknown>[]
+  ): INotificationBuilder<T> {
+    this.channels = channels;
+    return this;
+  }
 
-        this.content = content;
-        return this;
-    }
+  withContent(content: T): INotificationBuilder<T> {
+    this.content = content;
+    return this;
+  }
 
-    withStatus(status: NotificationStatus): INotificationBuilder<T> {
-        this.status = status;
-        return this;
-    }
+  withStatus(status: NotificationStatus): INotificationBuilder<T> {
+    this.status = status;
+    return this;
+  }
 }
+
+export const NotificationBuilderSymbol = Symbol(NotificationBuilder.name);

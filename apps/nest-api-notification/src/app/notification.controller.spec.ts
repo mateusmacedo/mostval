e15fca6d6ae5eaf7aedfab1940651ea6ae7d8455
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 15:55:09 -0300

    test(notification): 🧪 enhance unit tests for NotificationController
    - Updated the NotificationController unit tests to include a mock service for improved isolation and reliability.
    - Added a new test case for the notify method, ensuring that notifications are created and persisted correctly.
    - Enhanced type safety by incorporating TCreateNewNotificationInput in the tests, contributing to better maintainability and clarity within the notification module.

--- apps/nest-api-notification/src/app/notification.controller.spec.ts
+++ apps/nest-api-notification/src/app/notification.controller.spec.ts
@@ -1,21 +1,56 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { NotificationController } from './notification.controller'
-import { NotificationService } from './notification.service'
+import { TCreateNewNotificationInput } from '@mostval/notification';
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotificationController } from './notification.controller';
+import { NotificationService } from './notification.service';
+
+const mockNotificationService = {
+  createAndPersistNotification: jest.fn(),
+};
 
 describe('NotificationController', () => {
-  let app: TestingModule
+  let app: TestingModule;
+  let service: NotificationService;
+  let controller: NotificationController;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [NotificationController],
-      providers: [NotificationService],
-    }).compile()
-  })
+      providers: [
+        {
+          provide: NotificationService,
+          useValue: mockNotificationService,
+        },
+      ],
+    }).compile();
 
-  describe('getData', () => {
-    it('should return "Notification API"', () => {
-      const appController = app.get<NotificationController>(NotificationController)
-      expect(appController.get()).toEqual({ message: 'Notification API' })
-    })
-  })
-})
+    service = app.get<NotificationService>(NotificationService);
+    controller = app.get<NotificationController>(NotificationController);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
+  });
+
+  describe('notify', () => {
+    it('should create and persist a notification', async () => {
+      const data: TCreateNewNotificationInput<string> = {
+        channels: [],
+        content: 'Test',
+      };
+
+      mockNotificationService.createAndPersistNotification.mockResolvedValue({
+        message: 'Notification API',
+      });
+
+      const result = await controller.notify(data);
+      expect(result).toEqual({
+        message: 'Notification API',
+      });
+    });
+  });
+});

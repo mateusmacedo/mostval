e0bbd1729ca8b161ae380b7eda604ceb0a0f10db
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 23:03:16 -0300

    refactor(user.provider): 🔄 update user factory and message store to utilize flexible types
    - Refactored UserFactoryProvider and MessageStoreProvider to implement TFlexible for credentials and metadata, enhancing type safety and flexibility.
    - Updated the useFactory method signatures to align with the new type structure, ensuring consistency across user creation and message handling.
    - Improved overall code maintainability by streamlining the provider implementations, promoting better organization and clarity in the user management logic.

--- apps/nest-api-users/src/app/user.provider.ts
+++ apps/nest-api-users/src/app/user.provider.ts
@@ -1,23 +1,39 @@
-import { IMessageStore, IMetadata, InMemoryMessageStore, IPayload, Message, MESSAGE_STORE } from '@mostval/common'
-import { InMemoryUserRepository, USER_FACTORY, USER_REPOSITORY, UserFactory, UserProps } from '@mostval/users'
-import { ICredentials } from '@mostval/iam'
+import {
+  BaseMetadata,
+  IMessageStore,
+  InMemoryMessageStore,
+  Message,
+  MESSAGE_STORE,
+  TFlexible,
+} from '@mostval/common';
+import { ICredentials } from '@mostval/iam';
+import {
+  InMemoryUserRepository,
+  USER_FACTORY,
+  USER_REPOSITORY,
+  UserFactory,
+} from '@mostval/users';
 
 export const UserFactoryProvider = {
   provide: USER_FACTORY,
-  useFactory: (msgStore: IMessageStore<Message<IPayload<ICredentials>, IMetadata>>) => {
-    return new UserFactory(msgStore)
+  useFactory: (
+    msgStore: IMessageStore<
+      Message<TFlexible<ICredentials>, TFlexible<BaseMetadata>>
+    >
+  ) => {
+    return new UserFactory(msgStore);
   },
   inject: [MESSAGE_STORE],
-}
+};
 
 export const MessageStoreProvider = {
   provide: MESSAGE_STORE,
   useFactory: () => {
-    return new InMemoryMessageStore()
+    return new InMemoryMessageStore();
   },
-}
+};
 
 export const UserRepositoryProvider = {
   provide: USER_REPOSITORY,
   useClass: InMemoryUserRepository,
-}
+};

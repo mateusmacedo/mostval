32e10de7ffc57e01105bf2ba6d4de64ec25601c6
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 22:28:17 -0300

    refactor(message.spec): 🔄 update tests to align with new flexible type structure
    - Refactored the test cases in message.spec.ts to utilize the updated TFlexible type for payload and metadata, enhancing type safety.
    - Simplified the test setup by removing unnecessary properties from the metadata object, focusing on essential fields.
    - This change ensures that the tests accurately reflect the recent refactor of the Message class, improving clarity and maintainability.

--- packages/common/src/lib/message.spec.ts
+++ packages/common/src/lib/message.spec.ts
@@ -1,56 +1,39 @@
-import { Message, IMeta, IPayload } from './message'
+import { BaseMetadata, Message } from './message';
+import { TFlexible } from './utility-type';
 
-interface ITestPayload extends IPayload<string> {
-  value: string
+interface ITestPayload extends TFlexible<{ value: string }> {
+  value: string;
 }
-interface ITestMeta extends IMeta<string> {
-  createdBy: string
+interface ITestMeta extends TFlexible<BaseMetadata> {
+  createdBy: string;
 }
 
 class TestMessage extends Message<ITestPayload, ITestMeta> {}
 
 describe('Message', () => {
   it('should be able to create a message', () => {
-    const payload: ITestPayload = { value: 'test@example.com' }
-    const metadata: ITestMeta = {
-      createdBy: 'admin',
-      timestamp: Date.now().toString(),
-      id: '1',
-      schema: 'user',
-      type: 'new',
-    }
+    const payload: ITestPayload = { value: 'test' };
+    const metadata: ITestMeta = { createdBy: 'admin' };
 
-    const message = new TestMessage(payload, metadata)
-    expect(message).toBeDefined()
-  })
+    const message = new TestMessage(payload, metadata);
+    expect(message).toBeDefined();
+  });
 
   it('should be able to get the payload', () => {
-    const payload: ITestPayload = { value: 'test@example.com' }
-    const metadata: ITestMeta = {
-      createdBy: 'admin',
-      timestamp: Date.now().toString(),
-      id: '1',
-      schema: 'user',
-      type: 'new',
-    }
+    const payload: ITestPayload = { value: 'test' };
+    const metadata: ITestMeta = { createdBy: 'admin' };
 
-    const message = new TestMessage(payload, metadata)
-    expect(message.payload).toBeDefined()
-    expect(message.payload).toEqual(payload)
-  })
+    const message = new TestMessage(payload, metadata);
+    expect(message.payload).toBeDefined();
+    expect(message.payload).toEqual(payload);
+  });
 
   it('should be able to get the metadata', () => {
-    const payload: ITestPayload = { value: 'test@example.com' }
-    const metadata: ITestMeta = {
-      createdBy: 'admin',
-      timestamp: Date.now().toString(),
-      id: '1',
-      schema: 'user',
-      type: 'new',
-    }
+    const payload: ITestPayload = { value: 'test' };
+    const metadata: ITestMeta = { createdBy: 'admin' };
 
-    const message = new TestMessage(payload, metadata)
-    expect(message.metadata).toBeDefined()
-    expect(message.metadata).toEqual(metadata)
-  })
-})
+    const message = new TestMessage(payload, metadata);
+    expect(message.metadata).toBeDefined();
+    expect(message.metadata).toEqual(metadata);
+  });
+});

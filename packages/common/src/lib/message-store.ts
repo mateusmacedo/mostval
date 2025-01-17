dbd918c3c15fe59bc9a241eb94665e702bcd1f63
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-16 22:28:28 -0300

    refactor(message-store): 🔄 enhance IMessageStore interface with flexible types
    - Updated the IMessageStore interface and InMemoryMessageStore class to utilize the TFlexible type for payload and metadata, improving type safety and clarity.
    - Refactored method signatures for add, get, and remove to align with the new flexible type structure, ensuring consistency across the message handling logic.
    - Enhanced the matches method to accommodate the updated criteria types, promoting better filtering capabilities within the message store.
    - This refactor aligns with recent changes in the message structure, enhancing maintainability and readability of the codebase.

--- packages/common/src/lib/message-store.ts
+++ packages/common/src/lib/message-store.ts
@@ -1,41 +1,56 @@
-import { IMetadata, IPayload, Message } from '@mostval/common';
+import { Message } from './message';
+import { TFlexible } from './utility-type';
 
-export interface IMessageStore<T extends Message<IPayload<unknown>, IMetadata>> {
-  add(message: T): void
-  get(criteria: Partial<T> | Partial<IMetadata>): T[]
-  remove(criteria: Partial<T> | Partial<IMetadata>): void
+export interface IMessageStore<
+  T extends Message<TFlexible<unknown>, TFlexible<unknown>>,
+> {
+  add(message: T): void;
+  get(criteria: Partial<T> | Partial<TFlexible<unknown>>): T[];
+  remove(criteria: Partial<T> | Partial<TFlexible<unknown>>): void;
 }
 
-export class InMemoryMessageStore<T extends Message<IPayload<unknown>, IMetadata>>
-  implements IMessageStore<T>
+export class InMemoryMessageStore<
+  T extends Message<TFlexible<unknown>, TFlexible<unknown>>,
+> implements IMessageStore<T>
 {
-  private messages: T[] = []
+  private messages: T[] = [];
 
   add(message: T): void {
-    this.messages.push(message)
+    this.messages.push(message);
   }
 
-  get(criteria: Partial<T> | Partial<IMetadata>): T[] {
-    return this.messages.filter((message) => this.matches(message, criteria)) as T[]
+  get(criteria: Partial<T> | Partial<TFlexible<unknown>>): T[] {
+    return this.messages.filter((message) =>
+      this.matches(message, criteria)
+    ) as T[];
   }
 
-  remove(criteria: Partial<T> | Partial<IMetadata>): void {
-    this.messages = this.messages.filter((message) => !this.matches(message, criteria))
+  remove(criteria: Partial<T> | Partial<TFlexible<unknown>>): void {
+    this.messages = this.messages.filter(
+      (message) => !this.matches(message, criteria)
+    );
   }
 
-  private matches(message: T, criteria: Partial<T> | Partial<IMetadata>): boolean {
+  private matches(
+    message: T,
+    criteria: Partial<T> | Partial<TFlexible<unknown>>
+  ): boolean {
     if (!message.metadata) {
-      return false
+      return false;
     }
     return Object.entries(criteria).every(([key, value]) => {
       if (key === 'payload' && message.payload && typeof value === 'object') {
         return Object.entries(value).every(
-          ([pKey, pValue]) => (message.payload as { [key: string]: any })[pKey] === pValue,
-        )
+          ([pKey, pValue]) =>
+            (message.payload as { [key: string]: any })[pKey] === pValue
+        );
       }
-      return key in message.metadata && (message.metadata as { [key: string]: any })[key] === value
-    })
+      return (
+        key in message.metadata &&
+        (message.metadata as { [key: string]: any })[key] === value
+      );
+    });
   }
 }
 
-export const MESSAGE_STORE = Symbol('IMessageStore')
+export const MESSAGE_STORE = Symbol('IMessageStore');
